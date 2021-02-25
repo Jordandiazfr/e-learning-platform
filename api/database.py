d022e2d754ef2feeed043f652abb43909f252c66
Jozuah <galbas.joshua@gmail.com>
2021-02-25 10:51:55 +0100

    fake playlist + insert into db - done

--- api/database.py
+++ api/database.py
@@ -54,7 +54,7 @@ class Create_and_set_database():
         except Error as e :
             logging.warning("[MYSQL] message error:  %s", (e))
 
-    def createTableSpecialName(self,support_name) :
+    def createTableSpecialName(self,course_name) :
         self.conn = mysql.connector.connect(host='dbserver',
                         user='root',
                         database='DB_LEARNING',
@@ -64,57 +64,67 @@ class Create_and_set_database():
             sql_query = self.conn.cursor()
 
             #Création de la table en argument
-            sql_query.execute(f"DROP TABLE IF EXISTS {support_name}")    
-            sql_query.execute(f"""CREATE TABLE IF NOT EXISTS {support_name} (id_value INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            sql_query.execute(f"DROP TABLE IF EXISTS {course_name}")    
+            sql_query.execute(f"""CREATE TABLE IF NOT EXISTS {course_name} (id_value INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
              link VARCHAR(100) NOT NULL,
              niveau VARCHAR(100) NOT NULL,
              titre VARCHAR(100) NOT NULL,
              description TEXT,
              tags VARCHAR(100) NOT NULL,
              rate INT NOT NULL)""")
-            logging.info("[MYSQL] table  %s has been successfully created ", (support_name) )
+            logging.info("[MYSQL] table  %s has been successfully created ", (course_name) )
         except Error as e :
             logging.warning("[MYSQL] message error:  %s", (e))   
 
-def enAttente():
+    def fetchDATAfile(self,course_name):
+        liste_link = []
+        liste_level = []
+        liste_title = []
+        liste_desc = []
+        liste_tags = []
+        liste_rate = []
+        with open('data_course.txt') as json_file:
+            data = json.load(json_file)
+            for i in range (0,len(data[course_name])):
+                #pour add le link
+                liste_link.append(data[course_name][i][1])
+                #pour add le niveau
+                liste_level.append(data[course_name][i][2])
+                #pour add le title
+                liste_title.append(data[course_name][i][3])
+                #pour add la description
+                liste_desc.append(data[course_name][i][4])
+                #pour add les tags
+                liste_tags.append(data[course_name][i][5])
+                #pour add le rating
+                liste_rate.append(data[course_name][i][6])
+        self.full_course_info = list(zip(liste_link,liste_level,liste_title,liste_desc,liste_tags,liste_rate))
+        return self.full_course_info
+
+    def setIntoTableCourse(self,course_name,course_info):
+        self.conn = mysql.connector.connect(host='dbserver',
+                                user='root',
+                                database='DB_LEARNING',
+                                password='123',
+                                )
+        sql_query = self.conn.cursor()
+        insert = f"INSERT INTO {course_name} (link, niveau, titre, description, tags, rate) VALUES (%s, %s, %s, %s, %s, %s);"
+        value = course_info
+
+        sql_query.executemany(insert,value)
+        self.conn.commit()
+
+def main():
     reset_logfile("log.txt")
     setDB = Create_and_set_database()
     setDB.createTableCours()
-    setDB.createTableSpecialName("Python")
-    setDB.createTableSpecialName("Azure")
+    setDB.createTableSpecialName("PYTHON")
+    setDB.createTableSpecialName("AZURE")
+    setDB.createTableSpecialName("SRE")
+    setDB.setIntoTableCourse("PYTHON",setDB.fetchDATAfile("python"))
+    setDB.setIntoTableCourse("AZURE",setDB.fetchDATAfile("azure"))
+    setDB.setIntoTableCourse("SRE",setDB.fetchDATAfile("sre"))
 
-def fetchDATAfile(course_name):
-    liste_link = []
-    liste_level = []
-    liste_title = []
-    liste_desc = []
-    liste_tags = []
-    liste_rate = []
-    with open('data_course.txt') as json_file:
-        data = json.load(json_file)
-        for i in range (0,len(data[course_name])):
-            #pour add le link
-            print(data[course_name][i][1])
-            liste_link.append(data[course_name][i][1])
-            #pour add le niveau
-            print(data[course_name][i][2])
-            liste_level.append(data[course_name][i][2])
-            #pour add le title
-            print(data[course_name][i][3])
-            liste_title.append(data[course_name][i][3])
-            #pour add la description
-            print(data[course_name][i][4])
-            liste_desc.append(data[course_name][i][4])
-            #pour add les tags
-            print(data[course_name][i][5])
-            liste_tags.append(data[course_name][i][5])
-            #pour add le rating
-            print(data[course_name][i][6])
-            liste_rate.append(data[course_name][i][6])
-    print(liste_link)
-
-fetchDATAfile("python")
-
-
+main()
 
 
